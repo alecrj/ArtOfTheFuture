8644d6da6dce1101756a76e49bd493b18d7ed1cd
Alec <99alecrodriguez@gmail.com>
2025-07-07 15:06:56 -0400

    Initial Commit

---
+++ ArtOfTheFuture/ArtOfTheFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtOfTheFutureApp.swift
+//  ArtOfTheFuture
+//
+//  Created by Alec on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtOfTheFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtOfTheFutureTests/ArtOfTheFutureTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtOfTheFutureTests.swift
+//  ArtOfTheFutureTests
+//
+//  Created by Alec on 7/7/25.
+//
+
+import Testing
+@testable import ArtOfTheFuture
+
+struct ArtOfTheFutureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
